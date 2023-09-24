7485a6abbb6918c8f1566db24a868c81ca5f2bf4
Denis <foxtrot13.88@yandex.ru>
2023-09-25 00:02:41 +1000

    Refactor: concurency errors

--- internal/task/Google/task.go
+++ internal/task/Google/task.go
@@ -9,6 +9,7 @@ import (
 
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
+	"golang.org/x/sync/errgroup"
 	"google.golang.org/api/calendar/v3"
 	"google.golang.org/api/option"
 	"google.golang.org/api/tasks/v1"
@@ -17,6 +18,10 @@ import (
 type TaskResult struct {
 	Summary string
 	Date    string
+}
+
+func (s TaskResult) String() string {
+	return fmt.Sprintf("%s - %s", s.Summary, s.Date)
 }
 
 type GCalendar struct {
@@ -90,103 +95,112 @@ func New(ctx context.Context, tokenfile, clientsecret string) (*GCalendar, error
 
 func (g *GCalendar) Info(calendarid string) ([]TaskResult, error) {
 
-	errorch := make(chan error)
-	defer close(errorch)
+	errgr, ctx := errgroup.WithContext(context.Background())
 
-	resultcal := g.infoCal(calendarid, errorch)
-	resulttask := g.infoTasks(errorch)
+	resultcal := make(chan TaskResult)
+	errgr.Go(func() error {
+		defer close(resultcal)
+		if err := g.infoCal(calendarid, resultcal, ctx); err != nil {
+			return err
+		}
+		return nil
+	})
+
+	resulttask := make(chan TaskResult)
+	errgr.Go(func() error {
+		defer close(resulttask)
+		if err := g.infoTasks(resulttask, ctx); err != nil {
+			return err
+		}
+		return nil
+	})
 
 	resultch := merge(resultcal, resulttask)
+
 	result := []TaskResult{}
-	for {
-		select {
-		case err := <-errorch:
-			return nil, err
-		case item, ok := <-resultch:
-			if !ok {
-				return result, nil
-			}
-			result = append(result, item)
-		}
+	for item := range resultch {
+		result = append(result, item)
+	}
+
+	if err := errgr.Wait(); err != nil {
+		return nil, err
+	} else {
+		return result, nil
 	}
 
 }
 
-func (g *GCalendar) infoCal(calendarid string, errorch chan<- error) <-chan TaskResult {
+func (g *GCalendar) infoCal(calendarid string, out chan<- TaskResult, ctx context.Context) error {
 
-	out := make(chan TaskResult)
+	curtime := time.Now()
+	tstart := time.Date(curtime.Year(), curtime.Month(), curtime.Day(), 0, 0, 0, 0, curtime.Location()).Format(time.RFC3339)
+	tend := time.Date(curtime.Year(), curtime.Month(), curtime.Day(), 23, 59, 59, 59, curtime.Location()).Format(time.RFC3339)
 
-	go func() {
-		defer close(out)
-		curtime := time.Now()
-		tstart := time.Date(curtime.Year(), curtime.Month(), curtime.Day(), 0, 0, 0, 0, curtime.Location()).Format(time.RFC3339)
-		tend := time.Date(curtime.Year(), curtime.Month(), curtime.Day(), 23, 59, 59, 59, curtime.Location()).Format(time.RFC3339)
-		events, err := g.calendarService.Events.List(calendarid).ShowDeleted(false).
-			SingleEvents(true).TimeMin(tstart).TimeMax(tend).MaxResults(10).OrderBy("startTime").Do()
-		if err != nil {
-			errorch <- err
-			return
+	events, err := g.calendarService.Events.List(calendarid).ShowDeleted(false).
+		SingleEvents(true).TimeMin(tstart).TimeMax(tend).MaxResults(10).OrderBy("startTime").Do()
+	if err != nil {
+		return err
+	}
+
+	for _, item := range events.Items {
+		var result TaskResult
+		if item.Start.DateTime == "" {
+			result.Date = "весь день"
+		} else {
+			date, _ := time.Parse(time.RFC3339, item.Start.DateTime)
+			result.Date = date.Format("15:04:05")
+		}
+		result.Summary = item.Summary
+
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case out <- result:
 		}
 
-		for _, item := range events.Items {
-			var result TaskResult
-			if item.Start.DateTime == "" {
-				result.Date = "весь день"
-			} else {
-				date, _ := time.Parse(time.RFC3339, item.Start.DateTime)
-				result.Date = date.Format("15:04:05")
-			}
-			result.Summary = item.Summary
-			out <- result
-		}
-	}()
+	}
 
-	return out
+	return nil
 
 }
 
-func (g *GCalendar) infoTasks(errorch chan<- error) <-chan TaskResult {
+func (g *GCalendar) infoTasks(out chan<- TaskResult, ctx context.Context) error {
 
-	out := make(chan TaskResult)
+	tasklists, err := g.taskService.Tasklists.List().Do()
+	if err != nil {
+		return err
+	}
 
-	go func() {
-		defer close(out)
-		tasklists, err := g.taskService.Tasklists.List().Do()
+	curtime := time.Now()
+	startofday := time.Date(curtime.Year(), curtime.Month(), curtime.Day(), 0, 0, 0, 0, time.UTC)
+	tend := startofday.AddDate(0, 0, 1).Format(time.RFC3339)
+	tstart := startofday.Format(time.RFC3339)
+
+	for _, tasklist := range tasklists.Items {
+
+		events, err := g.taskService.Tasks.List(tasklist.Id).ShowCompleted(false).
+			ShowDeleted(false).ShowHidden(false).DueMin(tstart).DueMax(tend).Do()
 		if err != nil {
-			errorch <- err
-			return
+			return err
 		}
 
-		curtime := time.Now()
-		startofday := time.Date(curtime.Year(), curtime.Month(), curtime.Day(), 0, 0, 0, 0, time.UTC)
-		tend := startofday.AddDate(0, 0, 1).Format(time.RFC3339)
-		tstart := startofday.Format(time.RFC3339)
+		if len(events.Items) > 0 {
+			for _, item := range events.Items {
+				var result TaskResult
+				result.Summary = item.Title
+				result.Date = "сегодня"
 
-		for _, tasklist := range tasklists.Items {
-
-			events, err := g.taskService.Tasks.List(tasklist.Id).ShowCompleted(false).
-				ShowDeleted(false).ShowHidden(false).DueMin(tstart).DueMax(tend).Do()
-			if err != nil {
-				errorch <- err
-				return
-			}
-
-			if len(events.Items) > 0 {
-				for _, item := range events.Items {
-					var result TaskResult
-					result.Summary = item.Title
-					result.Date = "сегодня"
-					out <- result
+				select {
+				case <-ctx.Done():
+					return ctx.Err()
+				case out <- result:
 				}
+
 			}
-
 		}
-	}()
 
-	return out
+	}
 
-}
+	return nil
 
-func (s TaskResult) String() string {
-	return fmt.Sprintf("%s - %s", s.Summary, s.Date)
 }
